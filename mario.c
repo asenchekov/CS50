c359828600bbce4fd67c6353f5b089fdd893cfca
asenchekov <asenchekov@users.noreply.github.com>
2018-08-07 00:51:53 +0000

    20180807T005131Z

--- mario.c
+++ mario.c
@@ -12,37 +12,37 @@ int main(void)
     int i = 1;
 
 //check if integer is positive and lower than 24
-do
-{
-    h = get_int("Positive number please, not bigger than 23: \n");
-
-    while (h >= 24)
+    do
     {
         h = get_int("Positive number please, not bigger than 23: \n");
+
+        while (h >= 24)
+        {
+            h = get_int("Positive number please, not bigger than 23: \n");
+        }
     }
-}
-while (h < 0);
+    while (h < 0);
 
 //draw the pyramide
-while (h > 0)
-{
+    while (h > 0)
+    {
 
 //drawing spaces first
-    for (j = 1; j < h; j++)
-    {
-        printf("%c", space);
+        for (j = 1; j < h; j++)
+        {
+            printf("%c", space);
 
-    }
+        }
 //drawing actual pyramide
-    for (int k = i + 1; k > 0; k--)
-    {
-        printf("%c", hash);
+        for (int k = i + 1; k > 0; k--)
+        {
+            printf("%c", hash);
 
-    }
+        }
 
-    printf("\n");
-    h--;
-    i++;
+        printf("\n");
+        h--;
+        i++;
 
     }
 
